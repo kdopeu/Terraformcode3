6f2d650462438a42c3373dd15c3dd0810a9cf301
kouekado <kdopeu71@gmail.com>
2023-10-04 10:18:58 -0400

    maint.tf change

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.61.0"
+    }
+
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+resource "aws_iam_group" "demo" {
+  name = "security"
+
+}
+resource "aws_iam_user" "user1" {
+  name = "serge2023"
+
+}
+
